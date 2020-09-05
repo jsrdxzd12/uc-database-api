c5574dcc841a984fe9b838a55643c32f3e01df8c
xzd <jsrdxzd@qq.com>
2020-09-05 12:32:09 +0800

    database

---
+++ src/main/java/org/mozi/xzd/api/database/UcCommonDbApiApplication.java
@@ -0,0 +1,13 @@
+package org.mozi.xzd.api.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcCommonDbApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UcCommonDbApiApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/org/mozi/xzd/api/database/UcCommonDbApiApplicationTests.java
@@ -0,0 +1,16 @@
+package org.mozi.xzd.api.database;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UcCommonDbApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
